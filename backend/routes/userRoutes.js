a1bceee4eea1c1f25dc61089cce0c7461a0d7d38
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-09-09 13:29:15 +0300

    fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,12 @@ import {
   login,
   registerUser,
   getUser,
-  updateUserProfile,
+  // updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/login").post(login);
 router.route("/register").post(registerUser);
-router.route("/profile").get(protect, getUser).put(protect, updateUserProfile);
+router.route("/profile").get(protect, getUser); // .put(protect, updateUserProfile);
 
 export default router;
